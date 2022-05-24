644378b105172497ec99a0492baee2c46a72c4e4
HanDaWoon <bookpia.han@gmail.com>
2022-05-24 18:00:00 +0900

    0524

---
+++ OOP_MenuClickApp/OOP_MenuClickApp/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_MenuClickApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void printPreviewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void customizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void contentsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void indexToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void searchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(((ToolStripMenuItem)sender).Text);
+        }
+    }
+}
